27dd2be112c1909093839dd991e3a3647f78e143
devgme <devang.goldmedalindia@gmail.com>
2023-05-18 15:32:27 +0530

    fix landing page 1

---
+++ assets/js/custom.js
@@ -0,0 +1,21 @@
+
+// Video jS
+
+$(function(){
+    var $refreshButton = $('#refresh');
+    var $results = $('#css_result');
+    
+    function refresh(){
+      var css = $('style.cp-pen-styles').text();
+      $results.html(css);
+    }
+  
+    refresh();
+    $refreshButton.click(refresh);
+    
+    // Select all the contents when clicked
+    $results.click(function(){
+      $(this).select();
+    });
+  });
+  
