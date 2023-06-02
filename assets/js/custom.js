d89133c9921f2f332d0878ff0af834e69fc269c7
devgme <devang.goldmedalindia@gmail.com>
2023-06-02 19:02:24 +0530

    todays final upload

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,23 +1,35 @@
-
 // Video jS
 
-$(function(){
-    var $refreshButton = $('#refresh');
-    var $results = $('#css_result');
-    
-    function refresh(){
-      var css = $('style.cp-pen-styles').text();
-      $results.html(css);
-    }
-  
-    refresh();
-    $refreshButton.click(refresh);
-    
-    // Select all the contents when clicked
-    $results.click(function(){
-      $(this).select();
-    });
-  });
-  
+$(function () {
+  var $refreshButton = $("#refresh");
+  var $results = $("#css_result");
 
-  
+  function refresh() {
+    var css = $("style.cp-pen-styles").text();
+    $results.html(css);
+  }
+
+  refresh();
+  $refreshButton.click(refresh);
+
+  // Select all the contents when clicked
+  $results.click(function () {
+    $(this).select();
+  });
+});
+
+
+
+    /*--
+        Bootstrap dropdown
+    -----------------------------------*/
+    // Add slideDown animation to Bootstrap dropdown when expanding.
+    $('.dropdown').on('show.bs.dropdown', function() {
+      $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+  });
+
+  // Add slideUp animation to Bootstrap dropdown when collapsing.
+  $('.dropdown').on('hide.bs.dropdown', function() {
+      $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+  });
+
